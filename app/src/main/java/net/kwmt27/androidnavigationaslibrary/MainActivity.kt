ff40a85719f6a88629adee7deec00244ba5ad103
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-09-10 13:41:20 +0900

    android libraryで navigation componentを使って画面遷移できることを確認

--- app/src/main/java/net/kwmt27/androidnavigationaslibrary/MainActivity.kt
+++ app/src/main/java/net/kwmt27/androidnavigationaslibrary/MainActivity.kt
@@ -1,11 +1,13 @@
 package net.kwmt27.androidnavigationaslibrary
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import net.kwmt27.mylibrary.MyLibMainActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,8 +17,7 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            startActivity(Intent(this, MyLibMainActivity::class.java))
         }
     }
 
